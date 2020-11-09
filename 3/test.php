5e503640cb04a50d78fbc153ffc7a695c26d36ac
diafree82 <diafree82@gmail.com>
2020-11-09 10:25:16 +0300

    Update test.php

--- 3/test.php
+++ 3/test.php
@@ -3,4 +3,34 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Morizo");
 ?>
 
+<?$APPLICATION->IncludeComponent(
+	"morizo:sale.basket.basket.line", 
+	".default", 
+	array(
+		"HIDE_ON_BASKET_PAGES" => "Y",
+		"PATH_TO_AUTHORIZE" => "",
+		"PATH_TO_BASKET" => SITE_DIR."personal/cart/",
+		"PATH_TO_ORDER" => SITE_DIR."personal/order/make/",
+		"PATH_TO_PERSONAL" => SITE_DIR."personal/",
+		"PATH_TO_PROFILE" => SITE_DIR."personal/",
+		"PATH_TO_REGISTER" => SITE_DIR."login/",
+		"POSITION_FIXED" => "N",
+		"SHOW_AUTHOR" => "N",
+		"SHOW_EMPTY_VALUES" => "Y",
+		"SHOW_NUM_PRODUCTS" => "Y",
+		"SHOW_PERSONAL_LINK" => "Y",
+		"SHOW_PRODUCTS" => "Y",
+		"SHOW_REGISTRATION" => "N",
+		"SHOW_TOTAL_PRICE" => "Y",
+		"COMPONENT_TEMPLATE" => ".default",
+		"SHOW_DELAY" => "N",
+		"SHOW_NOTAVAIL" => "N",
+		"SHOW_IMAGE" => "Y",
+		"SHOW_PRICE" => "Y",
+		"SHOW_SUMMARY" => "N",
+		"MAX_IMAGE_SIZE" => "70"
+	),
+	false
+);?>
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
